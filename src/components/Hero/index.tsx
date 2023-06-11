691127a31debf99ebefb9ef5ad937a4ec1733975
Rhine <rhine.islam@cookietechltd.com>
2023-06-12 00:57:26 +0600

    Feature : Video added to Hero component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -16,27 +16,26 @@ const Hero = () => {
     <motion.section
       ref={targetRef}
       style={{ opacity: opacity }}
-      className="relative mb-[4rem] h-screen  text-white before:pointer-events-none before:fixed before:inset-0 before:z-0  before:opacity-40"
+      className="relative mb-[4rem] h-screen  text-white before:pointer-events-none before:fixed before:inset-0 before:z-0  before:opacity-40 flex justify-center items-center"
     >
       <motion.div
-        className="min-h-screen hero"
-        style={{
-          backgroundImage: `url(https://www.gamebyte.com/wp-content/uploads/2023/05/cff8c01e-7159attpsol.jpg)`,
-          position,
-        }}
+        style={{ scale: scale }}
+        className="absolute z-10 text-center hero-content text-neutral-content"
       >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <motion.div
-          style={{ scale: scale }}
-          className="text-center hero-content text-neutral-content"
-        >
-          <div className="max-w-5xl">
-            <h1 className="mb-5 font-extrabold text-transparent text-8xl bg-clip-text bg-gradient-to-r from-red-500 to-pink-800 font-flesh">
-              Bloodborne
-            </h1>
-          </div>
-        </motion.div>
+        <div className="max-w-9xl">
+          <h1 className="mb-5 font-extrabold text-transparent text-[250px] bg-clip-text bg-gradient-to-r from-red-500 to-pink-900 font-flesh ">
+            Bloodborne
+          </h1>
+        </div>
       </motion.div>
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute z-0 w-auto min-w-full min-h-screen max-w-none opacity-40 "
+      >
+        <source src="./assets/bg.mp4" type="video/mp4" />
+      </video>
     </motion.section>
   );
 };
